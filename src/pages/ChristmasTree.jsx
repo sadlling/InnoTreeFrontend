2dfc4578825aa54547a8117d7108f4dd2c479a40
sadlling <iharzhilnski@gmail.com>
2025-01-01 23:13:19 +0300

    fix clicks on an empty area

--- src/pages/ChristmasTree.jsx
+++ src/pages/ChristmasTree.jsx
@@ -7,31 +7,49 @@ import { DecorationForm } from "../components/DecorationForm/DecorationForm.jsx"
 export const ChristmasTree = () => {
   const [decorations, setDecorations] = useState([]);
   const [treeMask, setTreeMask] = useState(null);
+  const [imgWidth, setImgWidth] = useState(0);
+  const [imgHeight, setImgHeight] = useState(0);
+
+  const handleAddDecoration = ({ x, y, decoration, text }) => {
+    setDecorations([...decorations, { x, y, decoration, text }]);
+  };
 
   useEffect(() => {
     const img = new Image();
 
     img.src = "/TreeWithoutBackground.png"; // Маска: прозрачный фон, елка — непрозрачная
     img.onload = () => {
+      setImgWidth(img.width);
+      setImgHeight(img.height);
       const canvas = document.createElement("canvas");
       canvas.width = img.width;
       canvas.height = img.height;
       const ctx = canvas.getContext("2d");
       ctx.drawImage(img, 0, 0);
+
       setTreeMask(ctx.getImageData(0, 0, img.width, img.height));
     };
   }, []);
 
-  const handleAddDecoration = ({ x, y, decoration, text }) => {
-    setDecorations([...decorations, { x, y, decoration, text }]);
-  };
-
   const handleTreeClick = async (e) => {
     if (!treeMask) return;
-    const x = e.pageX;
-    const y = e.pageY;
+    // Получаем размеры контейнера
+    const treeElement = e.currentTarget;
+    const rect = treeElement.getBoundingClientRect();
+
+    // Вычисляем координаты клика относительно контейнера
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    // Масштабируем координаты на основе разрешения изображения
+    const scaleX = imgWidth / treeElement.offsetWidth;
+    const scaleY = imgHeight / treeElement.offsetHeight;
+
+    // Масштабируем координаты клика
+    const scaledX = Math.floor(x * scaleX);
+    const scaledY = Math.floor(y * scaleY);
     // // Проверяем, находится ли точка внутри непрозрачной области маски
-    const index = (Math.floor(y) * treeMask.width + Math.floor(x)) * 4;
+    const index = (scaledY * treeMask.width + scaledX) * 4;
     const alpha = treeMask.data[index + 3];
 
     if (alpha === 0) {
